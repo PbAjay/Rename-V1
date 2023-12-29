1b7e826766eb99e99f72761f1fe51aa291ebd630
AjayZ TG <106470358+PbAjay@users.noreply.github.com>
2023-12-29 20:05:35 +0530

    Updated Version Of The Code

--- bot/core/handlers/not_big.py
+++ bot/core/handlers/not_big.py
@@ -1,9 +1,8 @@
-# (c) @AbirHasan2005
-
 from configs import Config
 from bot.client import Client
 from pyrogram.types import Message
 from bot.core.db.database import db
+from typing import Optional, Union
 
 
 async def handle_not_big(
@@ -13,58 +12,46 @@ async def handle_not_big(
     file_name: str,
     editable: Message,
     upload_mode: str = "document",
-    thumb: str = None,
-):
-    reply_markup = m.reply_to_message.reply_markup \
-        if m.reply_to_message.reply_markup \
-        else None
+    thumb: Optional[str] = None,
+) -> None:
+    reply_markup = m.reply_to_message.reply_markup if m.reply_to_message.reply_markup else None
     _db_caption = await db.get_caption(m.from_user.id)
     apply_caption = await db.get_apply_caption(m.from_user.id)
+
     if (not _db_caption) and (apply_caption is True):
-        caption = m.reply_to_message.caption.markdown \
-            if m.reply_to_message.caption \
-            else "**Developer: @AbirHasan2005**"
+        caption = m.reply_to_message.caption.markdown if m.reply_to_message.caption else "**Developer: @AbirHasan2005**"
     elif _db_caption and (apply_caption is True):
         caption = _db_caption
     else:
         caption = ""
+
     parse_mode = "Markdown"
+    
     if thumb:
         _thumb = await c.download_media(thumb, f"{Config.DOWNLOAD_DIR}/{m.from_user.id}/{m.message_id}/")
     else:
         _thumb = None
+
     upload_as_doc = await db.get_upload_as_doc(m.from_user.id)
 
     if (upload_as_doc is False) and (upload_mode == "video"):
-        performer = None
-        title = None
-        duration = m.reply_to_message.video.duration \
-            if m.reply_to_message.video.duration \
-            else 0
-        width = m.reply_to_message.video.width \
-            if m.reply_to_message.video.width \
-            else 0
-        height = m.reply_to_message.video.height \
-            if m.reply_to_message.video.height \
-            else 0
+        performer: Optional[str] = None
+        title: Optional[str] = None
+        duration: int = m.reply_to_message.video.duration if m.reply_to_message.video.duration else 0
+        width: int = m.reply_to_message.video.width if m.reply_to_message.video.width else 0
+        height: int = m.reply_to_message.video.height if m.reply_to_message.video.height else 0
     elif (upload_as_doc is False) and (upload_mode == "audio"):
-        width = None
-        height = None
-        duration = m.reply_to_message.audio.duration \
-            if m.reply_to_message.audio.duration \
-            else None
-        performer = m.reply_to_message.audio.performer \
-            if m.reply_to_message.audio.performer \
-            else None
-        title = m.reply_to_message.audio.title \
-            if m.reply_to_message.audio.title \
-            else None
+        width: Optional[int] = None
+        height: Optional[int] = None
+        duration: Optional[int] = m.reply_to_message.audio.duration if m.reply_to_message.audio.duration else None
+        performer: Optional[str] = m.reply_to_message.audio.performer if m.reply_to_message.audio.performer else None
+        title: Optional[str] = m.reply_to_message.audio.title if m.reply_to_message.audio.title else None
     else:
-        duration = None
-        width = None
-        height = None
-        performer = None
-        title = None
+        duration: Optional[int] = None
+        width: Optional[int] = None
+        height: Optional[int] = None
+        performer: Optional[str] = None
+        title: Optional[str] = None
 
     await c.normal_rename(
         file_id,
@@ -81,5 +68,5 @@ async def handle_not_big(
         height=height,
         performer=performer,
         title=title
-    )
-
+        )
+    
